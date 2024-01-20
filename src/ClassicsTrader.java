7c88579ebb5ca56a71b51277bb95c2104b4638ca
AlexD2112 <atdanakis@gmail.com>
2024-01-20 13:39:15 -0600

    Got data grabber working.

--- src/ClassicsTrader.java
+++ src/ClassicsTrader.java
@@ -16,7 +16,7 @@ public class ClassicsTrader {
      * and storing it in a CSV file.
      */
     public ClassicsTrader() {
-        this(3);
+        this(100);
     }
     /**
      * Create new ClassicsTrader class grabbing numCars cars
@@ -41,6 +41,8 @@ public class ClassicsTrader {
 
         //Get links from the search page
         String[] links = getLinksOnPage(HTML);
+        links[0] = "https://www.classic-trader.com/uk/cars/listing/ferrari/testarossa/testarossa/1985/332971";
+        //Set first element to be https://www.classic-trader.com/uk/cars/listing/alfa-romeo/giulietta/giulietta-sz/1960/332964
         int page = 2;
         while (links.length < numCars) {
             String newHTML;
@@ -63,8 +65,14 @@ public class ClassicsTrader {
         //Grab Car Data and put it in the array
         int j = 0;
         for (String link : links) {
-            doubleArray[j] = grabCarData();
+            System.out.println((j+1) + "/" + numCars + " " + link);
+            doubleArray[j] = grabCarData(link);
             j++;
+        }
+
+        //Log double array
+        for (Object[] array : doubleArray) {
+            System.out.println(Arrays.toString(array));
         }
 
         return doubleArray;
@@ -112,10 +120,125 @@ public class ClassicsTrader {
         return links;
     }
 
-    //Returns, in order, Make, Model, Year, HP, Mileage, Price, and Description
-    private Object[] grabCarData() {
-        Object[] carArray = new Object[6];
+    //Returns, in order, Make, Model, Year, HP, Mileage, Price, and whether or not its "matching serial numbers" (i.e. no parts have been replaced)
+    private Object[] grabCarData(String link) {
+        Object[] carArray = new Object[7];
+
+        String HTML;
+        try {
+            HTML = Jsoup.connect(link).get().html();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        //System.out.println(HTML);
+
+        //Get Make and Model- String value of the class right after the class with value "Make" and "Model". In between there is a </dt> tag, and than the <dd class etc > header for the value
+        String make = getAttrValue(HTML, "Make");
+        String model = getAttrValue(HTML, "Model");
+        if (make.equals("Notprovided")) {
+            carArray[0] = "N/A";
+        } else {
+            carArray[0] = make;
+        }
+        if (model.equals("Notprovided")) {
+            carArray[1] = "N/A";
+        } else {
+            carArray[1] = model;
+        }
+
+        //Get Year
+        String year = getAttrValue(HTML, "Year of manufacture");
+        if (year.equals("Notprovided")) {
+            carArray[2] = "N/A";
+        } else {
+            carArray[2] = Integer.parseInt(year);
+        }
+
+        //Get HP- after found, needs to be split at / and than the second number is the HP
+        String hp = getAttrValue(HTML, "Power (kW/hp)");
+        if (hp.equals("Notprovided")) {
+            carArray[3] = "N/A";
+        } else {
+            if (hp.contains("Factory:")) {
+                hp = hp.split("Factory:")[1];
+                //Replace spaces and parentheses
+                hp = hp.replaceAll("\\(", "").replaceAll("\\)", "");
+            }
+            carArray[3] = Integer.parseInt(hp.split("/")[1]);
+        }
+
+        //Get Mileage
+        String mileageStr = getAttrValue(HTML, "Mileage (read)");
+        if (mileageStr.equals("Notprovided")) {
+            carArray[4] = "N/A";
+        } else {
+            //If contains mi, remove mi and convert to int converted to km
+            //Otherwise just remove km
+            int mileage = getMileage(mileageStr);
+            carArray[4] = mileage;
+        }
+
+        //Get Price
+        if (HTML.contains("Price on request")) {
+            carArray[5] = "N/A";
+        } else {
+            //Search for pound symbol or euro symbol, grab value after
+            boolean inEuro = false;
+            int priceIndex = HTML.indexOf("£");
+            if (priceIndex == -1) {
+                priceIndex = HTML.indexOf("€");
+                inEuro = true;
+            }
+            int price = Integer.parseInt(HTML.substring(priceIndex + 1, HTML.indexOf("<", priceIndex)).replaceAll(",", ""));
+            if (inEuro) {
+                price = (int) (price * 0.86);
+            }
+            carArray[5] = price;
+        }
+
+        //Get whether or not its matching serial numbers
+        String matchingStatement = getAttrValue(HTML, "Matching numbers");
+        if (matchingStatement.equals("Notprovided")) {
+            carArray[6] = "N/A";
+        } else {
+            boolean matching = matchingStatement.equals("Yes");
+            //If matching is null, print out matching statement
+            carArray[6] = matching;
+        }
+
+
         return carArray;
+    }
+
+    private static int getMileage(String mileageStr) {
+        int mileage;
+        if (mileageStr.contains("mls")) {
+            mileage = (int) (Integer.parseInt(mileageStr.replaceAll("mls", "").replaceAll(",", "")) * 1.60934);
+        } else {
+            mileage = Integer.parseInt(mileageStr.replaceAll("km", "").replaceAll(",", ""));
+        }
+        return mileage;
+    }
+
+    private String getAttrValue(String HTML, String attr) {
+        int attrIndex = HTML.indexOf(attr);
+        int dtCloseIndex = HTML.indexOf("</dt>", attrIndex);
+        int i = 0;
+        while (dtCloseIndex - attrIndex > 100) {
+            attrIndex = HTML.indexOf(attr, attrIndex + 1);
+            dtCloseIndex = HTML.indexOf("</dt>", attrIndex);
+            i++;
+            if (i > 25) {
+                System.out.println(attrIndex + " " + dtCloseIndex);
+                System.out.println(HTML.substring(attrIndex, dtCloseIndex+5));
+                throw new RuntimeException("Could not find attribute " + attr);
+            }
+        }
+        int dtEndIndex = HTML.indexOf(">", attrIndex) + 1;
+        int ddEndIndex = HTML.indexOf(">", dtEndIndex+1)+1;
+        int ddCloseIndex = HTML.indexOf("</dd>", ddEndIndex);
+        return HTML.substring(ddEndIndex, ddCloseIndex).replaceAll("\n", "").replaceAll("\t", "").replaceAll(" ", "");
     }
     /**
      * Get the path to the CSV file
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        ClassicsTrader classicsTrader = new ClassicsTrader();
+        ClassicsTrader classicsTrader = new ClassicsTrader(100);
     }
 }
